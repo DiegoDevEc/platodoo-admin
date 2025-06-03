37937c0316640aa192f8db75ce9f56c75e8418e4
Diego Acosta <diego.acosta@externos.bci.cl>
2025-06-03 10:46:01 -0500

    Ajustes de seguridad

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: 'https://api.playtodoo.com',
   platform: 'BACKOFFICE',  // URL para producción
-  googleClientId: '793484162388-7m75d527f6nc7utf69hrit6i8c0aqj4u.apps.googleusercontent.com'
+  googleClientId: 'GOOGLE_CLIENT_ID_PLACEHOLDER'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:8080' ,
-  platform: 'BACKOFFICE', // URL del backend
-  googleClientId: '793484162388-7m75d527f6nc7utf69hrit6i8c0aqj4u.apps.googleusercontent.com',
-  googleSecret: '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'
+  platform: 'BACKOFFICE',  // URL para producción
+  googleClientId: 'GOOGLE_CLIENT_ID_PLACEHOLDER'
 };
